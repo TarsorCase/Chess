a8eb1ef410cef5f47c7d9f6abeb641eeb9300d37
Tars <97172661+TarsorCase@users.noreply.github.com>
2024-05-09 08:03:38 -0400

    Chages to main

--- src/main.py
+++ src/main.py
@@ -5,6 +5,7 @@ from const import *
 from game import Game
 from square import Square
 from move import Move
+import copy
 
 
 class Main:
@@ -14,6 +15,7 @@ class Main:
         self.screen = pygame.display.set_mode((WIDTH, HEIGHT))
         pygame.display.set_caption('Chess')
         self.game = Game()
+        self.computer_player = False  # Variable to track if playing against computer
 
     def mainloop(self):
 
@@ -21,6 +23,79 @@ class Main:
         game = self.game
         board = self.game.board
         dragger = self.game.dragger
+
+        def machine_move(boardCopy):
+            max_value = -99999
+            movement = ""
+            legal_moves = boardCopy.legal_moves()
+            for move in legal_moves:
+                result = alphabeta_pruning(copy.deepcopy(boardCopy), move, 3, -999999, 9999999, False)
+                if result > max_value:
+                    movement = move
+                    max_value = result
+            return movement
+
+        def alphabeta_pruning(boardCopy, movement, depth, alpha, beta, maximizingPlayer):
+            if depth == 0:
+                return evaluateBoard(boardCopy, movement)
+
+            boardCopy.push(chess.Move.from_uci(movement))
+            legal_moves = [str(mov) for mov in boardCopy.legal_moves]
+
+            if maximizingPlayer:
+                value = -999999
+                for move in legal_moves:
+                    value = max(value, alphabeta_pruning(boardCopy.copy(), move, depth - 1, alpha, beta, False))
+                    if value >= beta:
+                        break
+                    alpha = max(alpha, value)
+                return value
+            else:
+                value = 999999
+                for move in legal_moves:
+                    value = min(value, alphabeta_pruning(boardCopy.copy(), move, depth - 1, alpha, beta, True))
+                    if value <= alpha:
+                        break
+                    beta = min(beta, value)
+                return value
+
+        def evaluateBoard(boardCopy, movement):
+            value = 0
+            boardCopy.push(chess.Move.from_uci(movement))
+            for i in range(8):
+                for j in range(8):
+                    piece = str(boardCopy.piece_at(chess.Square((i * 8 + j))))
+                    value += getValueOfPiece(piece)
+            return value
+
+        def getValueOfPiece(letter):
+            if letter == 'r':
+                return 50
+            if letter == 'n':
+                return 30
+            if letter == 'b':
+                return 30
+            if letter == 'q':
+                return 90
+            if letter == 'k':
+                return 900
+            if letter == 'p':
+                return 10
+
+            if letter == 'R':
+                return -50
+            if letter == 'N':
+                return -30
+            if letter == 'B':
+                return -30
+            if letter == 'Q':
+                return -90
+            if letter == 'K':
+                return -900
+            if letter == 'P':
+                return -10
+
+            return 0
 
         while True:
             # show methods
@@ -37,24 +112,29 @@ class Main:
 
                 # click
                 if event.type == pygame.MOUSEBUTTONDOWN:
-                    dragger.update_mouse(event.pos)
+                    if not self.computer_player:  # Human vs Human
+                        dragger.update_mouse(event.pos)
 
-                    clicked_row = dragger.mouseY // SQSIZE
-                    clicked_col = dragger.mouseX // SQSIZE
+                        clicked_row = dragger.mouseY // SQSIZE
+                        clicked_col = dragger.mouseX // SQSIZE
 
-                    # if clicked square has a piece ?
-                    if board.squares[clicked_row][clicked_col].has_piece():
-                        piece = board.squares[clicked_row][clicked_col].piece
-                        # valid piece (color) ?
-                        if piece.color == game.next_player:
-                            board.calc_moves(piece, clicked_row, clicked_col, bool=True)
-                            dragger.save_initial(event.pos)
-                            dragger.drag_piece(piece)
-                            # show methods
-                            game.show_bg(screen)
-                            game.show_last_move(screen)
-                            game.show_moves(screen)
-                            game.show_pieces(screen)
+                        # if clicked square has a piece ?
+                        if board.squares[clicked_row][clicked_col].has_piece():
+                            piece = board.squares[clicked_row][clicked_col].piece
+                            # valid piece (color) ?
+                            if piece.color == game.next_player:
+                                board.calc_moves(piece, clicked_row, clicked_col, bool=True)
+                                dragger.save_initial(event.pos)
+                                dragger.drag_piece(piece)
+                                # show methods
+                                game.show_bg(screen)
+                                game.show_last_move(screen)
+                                game.show_moves(screen)
+                                game.show_pieces(screen)
+                    else:  # Human vs Computer
+                        # Computer makes a move
+                        movement = machine_move(board)
+                        board.push(chess.Move.from_uci(movement))
 
                 # mouse motion
                 elif event.type == pygame.MOUSEMOTION:
@@ -119,6 +199,10 @@ class Main:
                         game = self.game
                         board = self.game.board
                         dragger = self.game.dragger
+
+                    # Toggle between Human vs Human and Human vs Computer modes
+                    if event.key == pygame.K_c:
+                        self.computer_player = not self.computer_player
 
                 # quit application
                 elif event.type == pygame.QUIT:
